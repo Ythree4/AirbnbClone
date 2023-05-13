2a4710b812311a516831331d43243b5c74fd878e
Ythree4 <132032713+Ythree4@users.noreply.github.com>
2023-05-13 10:24:46 +0800

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import "./globals.css";
+import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal"
+import ToasterProvider from "./provider/ToasterProvider";
+
+export const metadata = {
+  title: "Gerome Portfolio",
+  description: "Airbnb Clone",
+};
+
+const font = Nunito({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+        <ToasterProvider />
+        <RegisterModal />
+        <Navbar />
+        </ClientOnly>
+        {children}
+        </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <div className="text-rose-500 text-5xl text-center">Hello Hiring Manger!</div>
+  )
+}
